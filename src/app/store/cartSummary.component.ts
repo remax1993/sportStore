cbe2297ee3fbbaa0faf8db2c539e3e95796da976
remax1993 <remax1993@gmail.com>
2019-05-23 12:32:48 +0300

    product list, sorting by category, 1 basket version

---
+++ src/app/store/cartSummary.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {Cart} from '../model/cart.model';
+
+@Component({
+    selector: 'cart-summary',
+    //moduleId: module.id,
+    templateUrl: 'cartSummary.component.html',
+    styleUrls: ['cartSummary.component.scss']
+})
+
+export class CartSummaryComponent {
+
+    constructor(public cart: Cart) {}
+}
