536427c552ba89c23b365f032125219faf5cef5f
remax1993 <remax1993@gmail.com>
2019-06-03 23:43:55 +0300

    routing, guard route, cart component

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import {ProductRepository} from './product.repository';
 import {StaticDataSource} from './static.datasource';
 import {Cart} from './cart.model';
+import {OrderRepository} from './order.repository';
+import {Order} from './order.model';
 
 @NgModule({
     providers: [
         ProductRepository,
         StaticDataSource,
-        Cart
+        Cart,
+        Order,
+        OrderRepository
     ]
 })
 export class ModelModule { }
--- src/app/storeFirst.guard.ts
+++ src/app/storeFirst.guard.ts
@@ -10,7 +10,7 @@ export class StoreFirstGuard {
 
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot
-                ):boolean {
+                                            ):boolean {
         if(this.firstNavigation){
             this.firstNavigation = false;
             if(route.component != StoreComponent){
